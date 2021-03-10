e463c31b154254a610be0c78022d4b5fa4bba719
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-10 17:02:23 +0000

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
+        'jest/globals': true,
+      },
+    extends: 'eslint:recommended',
+    extends: ['plugin:jest/recommended'],
+    parserOptions: {
+        ecmaVersion: 12,
+      },
+    rules: {
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
+      },
+    plugins: ['jest'],
+  };
