01c2d40422e6e4c80653db2a7245398c66c6241b
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-10 16:05:13 +0000

    Added feature test for shop and refactored Shop class

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,28 +1,59 @@
-const { Shop, item } = require('../src/gilded_rose');
+const { Shop } = require('../src/shop');
 const { NormalItem } = require('../src/normal_item');
 const { AgedBrie } = require('../src/aged_brie');
 const { Sulfuras } = require('../src/sulfuras');
+const { BackstagePasses } = require('../src/backstage_Passes');
 
 describe('Gilded Rose', function () {
+  const gildedRose = new Shop([new NormalItem('normal', 20, 20),
+  new AgedBrie('brie', 20, 20),
+  new NormalItem('normal 2', -1, 10),
+  new Sulfuras('Sulfuras', 10, 10),
+  new BackstagePasses('Pass 1', 30, 10),
+  new BackstagePasses('Pass 2', 10, 10),
+  new BackstagePasses('Pass 3', 5, 10),
+  new BackstagePasses('Pass 4', -1, 10),
+  new AgedBrie('brie 2', 20, 50),
+
+  ]);
+  const items = gildedRose.updateQuality();
+
   it('Normal items decrease by 1 within sellin period', function () {
-    const gildedRose = new Shop([new NormalItem('normal', 20, 20), new AgedBrie('brie', 20, 20)]);
-    const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(19);
     expect(items[0].name).toEqual('normal');
     expect(items[0].sellIn).toEqual(19);
-
-    // expect(items[1].quality).toEqual(21);
-    expect(items[1].name).toEqual('brie');
-    expect(items[1].sellIn).toEqual(19);
-
   });
 
-  xit('Aged brie increases by 1 as it ages', function () {
-    const gildedRose = new Shop([new AgedBrie('brie', 20, 20)]);
-    const items = gildedRose.updateQuality();
+  it('Aged brie increases by 1 as it ages', function () {
     expect(items[1].quality).toEqual(21);
-    expect(items[1].name).toEqual('brie');
-    expect(items[1].sellIn).toEqual(19);
+  });
+
+  it('Normal items decrease by 2 when out of sellin period', function () {
+    expect(items[2].quality).toEqual(8);
+  });
+
+  it('Sulfuras does not change', function () {
+    expect(items[3].quality).toEqual(10);
+  });
+
+  it('Backstage passes quality increase by 1 when 11 or more days left', function () {
+    expect(items[4].quality).toEqual(11);
+  });
+
+  it('Backstage passes quality increase by 2 when 10 to 6 days left', function () {
+    expect(items[5].quality).toEqual(12);
+  });
+
+  it('Backstage passes quality increase by 3 when 5 or less days left', function () {
+    expect(items[6].quality).toEqual(13);
+  });
+
+  it('Backstage passes quality reduce to 0 after the concert', function () {
+    expect(items[7].quality).toEqual(0);
+  });
+
+  it("No item's quality can be above 50", function () {
+    expect(items[8].quality).toEqual(50);
   });
 
 });
